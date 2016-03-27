e3d5371b22cdbb85acaa55bafc3397d946a5fe55
Jeff Ogata <jeffogata@outlook.com>
2016-03-27 08:17:55 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# aspnet-middleware-01-cli
+
+Custom middleware, accompanying the [ASP.NET Core Middleware](https://jeffogata.com/asp-net-core-middleware/) blog post.
+
+Tested with the following:
+* ASP.NET 5 RC1 Update1
+* Windows 10
+* Ubuntu 14.04
---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿namespace AspNetMiddleware
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.Extensions.Logging;
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.MinimumLevel = LogLevel.Warning;
+            loggerFactory.AddConsole(LogLevel.Warning);
+
+            app.UseArnoldQuotes();
+        }
+    }
+}
